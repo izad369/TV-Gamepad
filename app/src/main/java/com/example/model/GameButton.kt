23f637e88aeb466d6ba46f0de9b130a8351d69ad
izad369 <rezaei.reza1092@gmail.com>
2026-09-12 22:32:13 +0900

    restore: recover GameButton after accidental write

---
+++ app/src/main/java/com/example/model/GameButton.kt
@@ -0,0 +1,11 @@
+package com.example.model
+
+enum class GameButton {
+    A, B, X, Y,
+    Z,
+    UP, DOWN, LEFT, RIGHT,
+    L1, L2, R1, R2,
+    L3, R3,
+    SELECT, START, HOME, MENU,
+    UNKNOWN
+}
